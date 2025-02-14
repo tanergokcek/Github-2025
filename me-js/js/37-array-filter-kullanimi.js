66ccf95d7c46976ad9e7e92d71c07a51ed2404aa
Taner Gökçek <tanergokcek1@outlook.com>
2025-02-14 20:08:34 +0300

    10.Day

---
+++ me-js/js/37-array-filter-kullanimi.js
@@ -0,0 +1,21 @@
+//£££
+
+const PRODUCTS = ["Mic", "Cable", "Speaker", "Desktop Pc", "Server", "Mouse", "Keyboard" ]
+
+const NEW_PRODUCTS = PRODUCTS.filter(item =>
+             item.length > 5
+)
+console.log(NEW_PRODUCTS)
+
+const USERS = [
+            {fullName: "Ayse Sumer", isActive: false},
+            {fullName: "Ahmet Urgan", isActive: true},
+            {fullName: "Asya Basar", isActive: true},
+            {fullName: "Aksel Durmaz", isActive: false},
+]
+
+const ACTIVE_USERS = USERS.filter(user =>
+            //user.isActive yazsa da dogru.
+            user.isActive ===true    
+)
+console.log(ACTIVE_USERS);
---
+++ me-js/js/38-array-map-kullanimi.js
@@ -0,0 +1,26 @@
+
+const USERS = ["AYSE", "MehMet", "TugCE", "AkSEL"]
+
+const NEW_USERS = USERS.map(user =>
+            user.toLowerCase() 
+)
+console.log(NEW_USERS);
+
+
+/*//onemli !!!
+const USERS_OBJ = USERS.map(item =>
+{
+            return {
+                        userName: item, shortName: `${item[0]}.`,newName: `${item[0].toUpperCase()}${item.slice(1).toLowerCase()}`
+            }
+}
+)
+console.log(USERS_OBJ);
+*/
+
+//Ustekkinin daha kolayı.Returnsuz hali
+const USERS_OBJ = USERS.map( item => (
+            {userName: item, shortName: `${item[0]}.`,newName: `${item[0].toUpperCase()}${item.slice(1).toLowerCase()}`}
+)
+)
+console.log(USERS_OBJ);
