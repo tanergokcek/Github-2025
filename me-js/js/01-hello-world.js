3c3dc7cd02475e789867b473cfdde5092065ca19
Taner-Gokcekk <tanergokcek1@outlook.com>
2025-04-17 19:02:29 +0300

    a

--- me-js/js/01-hello-world.js
+++
@@ -1,5 +0,0 @@
-document.write('hi universe')                                           //£
-
-//alert('!!')                                                                       //£
-
-console.log('whats up universe')
--- me-js/js/11-prompt-ile-kullanimi.js
+++
@@ -1,8 +0,0 @@
-
-
-let fullName = prompt("Lutfen Adinizi Giriniz")
-console.log(fullName);
-
-let greeting = document.querySelector("#greeting")
-greeting.innerHTML = `${greeting.innerHTML}  <small style="color:red"> ${fullName} </small>
-`
